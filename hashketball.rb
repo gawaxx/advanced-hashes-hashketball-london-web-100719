f2aae0a336116a6f99adb474577014ca17a562c3
jules blanc <jules.blanc@unccub.com>
2019-10-04 08:40:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,11 +185,8 @@ end
 def player_numbers(team_name)
   teamnumbers = []
   game_hash.each {|location, team_info| 
-  
     if team_info[:team_name]==team_name 
-      
       team_info[:players].each {|player, player_info|
-      
         teamnumbers.push(player.values[0][:number])
     }
   end
